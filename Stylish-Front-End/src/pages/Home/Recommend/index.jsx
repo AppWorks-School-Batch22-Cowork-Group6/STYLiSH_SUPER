cd1def79f665e88e2030e0ed43c0976d3a5fa663
Sailwayfun <liaoleon000513@gmail.com>
2023-11-04 10:37:57 +0800

    Create Recommend component

---
+++ Stylish-Front-End/src/pages/Home/Recommend/index.jsx
@@ -0,0 +1,9 @@
+const Recommend = ({ children }) => {
+    return (
+        <div className="w-full h-[375px] pt-2 px-5 pb-5 bg-gray-300 rounded-md mb-4 flex flex-col relative gap-4 xl:hidden">
+            { children }
+        </div>
+    );
+};
+
+export default Recommend;
