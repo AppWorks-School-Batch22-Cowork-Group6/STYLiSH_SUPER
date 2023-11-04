340abf634df76d3116ffc8a3a3b131ec9382e2dd
Sailwayfun <liaoleon000513@gmail.com>
2023-11-04 09:12:18 +0800

    Create recommended products block

--- Stylish-Front-End/src/pages/Home/Products.jsx
+++ Stylish-Front-End/src/pages/Home/Products.jsx
@@ -146,18 +146,33 @@ function Products() {
 
   return (
     <Wrapper>
-      { products.map(({ id, main_image, colors, title, price }) => (
-        <Product key={ id } to={ `/products/${id}` }>
-          <ProductImage src={ main_image } />
-          <ProductColors>
-            { colors.map(({ code }) => (
-              <ProductColor $colorCode={ `#${code}` } key={ code } />
-            )) }
-          </ProductColors>
-          <ProductTitle>{ title }</ProductTitle>
-          <ProductPrice>TWD.{ price }</ProductPrice>
-        </Product>
-      )) }
+      { products.map(({ id, main_image, colors, title, price }, index) => {
+        return (
+          <>
+            <Product key={ id } to={ `/products/${id}` }>
+              <ProductImage src={ main_image } />
+              <ProductColors>
+                { colors.map(({ code }) => (
+                  <ProductColor $colorCode={ `#${code}` } key={ code } />
+                )) }
+              </ProductColors>
+              <ProductTitle>{ title }</ProductTitle>
+              <ProductPrice>TWD.{ price }</ProductPrice>
+            </Product>
+            { index === (products.length / 2 - 2) &&
+              <div className="w-full h-[200px] pt-2 px-3 pb-3 bg-gray-300 rounded-md mb-4 xl:hidden">
+                <div className="flex gap-1">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={ 1.5 } stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.362 5.214A8.252 8.252 0 0112 21 8.25 8.25 0 016.038 7.048 8.287 8.287 0 009 9.6a8.983 8.983 0 013.361-6.867 8.21 8.21 0 003 2.48z" />
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 18a3.75 3.75 0 00.495-7.467 5.99 5.99 0 00-1.925 3.546 5.974 5.974 0 01-2.133-1A3.75 3.75 0 0012 18z" />
+                  </svg>
+                  <span className="text-xl">大家都在買</span>
+                </div>
+              </div> }
+          </>
+
+        );
+      }) }
       { isLoading && <Loading type="spinningBubbles" color="#313538" /> }
     </Wrapper>
   );
