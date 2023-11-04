63ed31022e4648c35ae3478135394ff8bad9b42a
Sailwayfun <liaoleon000513@gmail.com>
2023-11-04 22:00:40 +0800

    Adjust padding and margin of mobile/desktop ver

--- Stylish-Front-End/src/pages/Home/Recommend/Button.jsx
+++ Stylish-Front-End/src/pages/Home/Recommend/Button.jsx
@@ -2,7 +2,7 @@ const Button = ({ position, onMoveToPrev, onMoveToNext }) => {
     const buttonPosition = position === 'left' ? '-left-4' : '-right-4';
     const buttonPath = position === 'left' ? 'M15.75 19.5L8.25 12l7.5-7.5' : 'M8.25 4.5l7.5 7.5-7.5 7.5';
     return (
-        <button className={ `absolute w-14 h-14 top-1/2 -translate-y-1/2 ${buttonPosition} z-10` }
+        <button className={ `absolute w-12 h-12 xl:w-14 xl:h-14 top-1/2 -translate-y-1/2 ${buttonPosition} z-10` }
             onClick={ position === "left" ?
                 onMoveToPrev :
                 onMoveToNext }>
--- Stylish-Front-End/src/pages/Home/Recommend/index.jsx
+++ Stylish-Front-End/src/pages/Home/Recommend/index.jsx
@@ -1,8 +1,8 @@
 const Recommend = ({ children, isProductPage }) => {
     const order = !isProductPage ? 'order-5' : 'order-none';
-    const margin = !isProductPage ? 'm-0' : 'm-5 xl:mx-0 xl:mt-14 xl:-mb-6';
+    const margin = !isProductPage ? 'mx-auto' : 'mx-5 xl:mx-0 xl:mt-14 xl:-mb-6';
     return (
-        <div className={ `w-full h-[375px] pt-2 px-10 pb-5 bg-gray-300 rounded-md mb-4 flex flex-col relative gap-4 overflow-hidden ${order} xl:mx-5 xl:mb-10 ${margin}` }>
+        <div className={ `w-4/5 xl:w-full h-[240px] xl:h-[375px] py-2 px-8 xl:px-10 xl:pb-5 bg-gray-300 rounded-md mb-4 flex flex-col relative gap-4 overflow-hidden ${order} xl:mx-5 xl:mb-10 ${margin}` }>
             { children }
         </div>
     );
