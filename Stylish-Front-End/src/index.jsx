efd78999f559a459eff238ca1e13724c5700f84b
Sailwayfun <liaoleon000513@gmail.com>
2023-11-05 09:31:07 +0800

    Add Live page

--- Stylish-Front-End/src/index.jsx
+++ Stylish-Front-End/src/index.jsx
@@ -7,19 +7,21 @@ import Home from "./pages/Home";
 import Product from "./pages/Product";
 import Profile from "./pages/Profile";
 import ThankYou from "./pages/ThankYou";
+import Live from "./pages/Live";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />}>
-        <Route index element={<Home />} />
-        <Route path="products/:id" element={<Product />} />
-        <Route path="checkout" element={<Checkout />} />
-        <Route path="thankyou" element={<ThankYou />} />
-        <Route path="profile" element={<Profile />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
+      <Route path="/" element={ <App /> }>
+        <Route index element={ <Home /> } />
+        <Route path="products/:id" element={ <Product /> } />
+        <Route path="checkout" element={ <Checkout /> } />
+        <Route path="thankyou" element={ <ThankYou /> } />
+        <Route path="profile" element={ <Profile /> } />
+        <Route path="live" element={ <Live /> } />
+        <Route path="*" element={ <Navigate to="/" replace /> } />
       </Route>
     </Routes>
   </BrowserRouter>
