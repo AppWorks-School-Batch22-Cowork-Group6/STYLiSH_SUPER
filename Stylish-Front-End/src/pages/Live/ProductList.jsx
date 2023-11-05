ffd08a7a638157ea658f624364d8754f588cbcfb
Sailwayfun <liaoleon000513@gmail.com>
2023-11-05 16:49:50 +0800

    Move controls into Video component

--- Stylish-Front-End/src/pages/Live/ProductList.jsx
+++ Stylish-Front-End/src/pages/Live/ProductList.jsx
@@ -2,13 +2,13 @@ import productImg from "../Home/product-main.png";
 import { Link } from "react-router-dom";
 const ProductList = () => {
     return (
-        <div className="row-start-2 px-6 xl:px-0 mt-6xl:m-0 xl:row-start-1 xl:col-start-2">
+        <div className="row-start-2 px-10 xl:px-0 mt-6xl:m-0 xl:row-start-1 xl:col-start-2">
             <h3 className="text-2xl xl:text-3xl mb-6 xl:mb-3">熱門商品</h3>
             <ul className="flex flex-col gap-5">
                 <li className="flex">
                     <img src={ productImg } alt="" className="w-20 xl:w-28 object-cover" />
                     <div className="flex flex-1 ml-2 xl:flex-col xl:ml-4">
-                        <p className="mt-0 mb-8 mr-10 xl:text-2xl leading-none">前開衩扭結洋裝</p>
+                        <p className="mt-0 mb-8 xl:text-2xl leading-none">前開衩扭結洋裝</p>
                         <p className="hidden xl:block xl:mb-14 xl:text-xl tracking-widest">TWD.799</p>
                     </div>
                     <div className="flex flex-col">
