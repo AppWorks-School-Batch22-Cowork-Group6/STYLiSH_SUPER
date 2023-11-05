e2b5ce93814dbf5e406de9051a3f623d6249d531
Sailwayfun <liaoleon000513@gmail.com>
2023-11-05 12:57:46 +0800

    Change background color

--- Stylish-Front-End/src/pages/Live/Wrapper.jsx
+++ Stylish-Front-End/src/pages/Live/Wrapper.jsx
@@ -1,6 +1,6 @@
 const Wrapper = ({ children }) => {
     return (
-        <div className="bg-gray-400 border-2 border-red-500 mx-auto max-w-[960px] pb-8 xl:pt-[65px] xl:pb-[49px] flex flex-wrap">
+        <div className="border-2 border-gray-500 mx-auto max-w-[960px] pb-8 xl:pt-[65px] xl:pb-[49px] flex flex-wrap">
             { children }
         </div>
     );
