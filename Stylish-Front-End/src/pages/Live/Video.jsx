50aa5a0cb525c35b528bc5218303d93386fc9071
Sailwayfun <liaoleon000513@gmail.com>
2023-11-05 18:17:09 +0800

    Create Controls component

--- Stylish-Front-End/src/pages/Live/Video.jsx
+++ Stylish-Front-End/src/pages/Live/Video.jsx
@@ -1,30 +1,13 @@
 import video from "./herbst_version2.mp4";
+import Controls from "./Controls";
+
 const Video = () => {
     return (
         <div className="h-full flex flex-col px-8 xl:gap-8 justify-center">
             <video muted autoPlay className="h-full bg-black px-10 xl:px-0">
                 <source src={ video } type="video/mp4" />
             </video>
-            <div className="flex gap-2 xl:gap-4 mt-2 pb-6">
-                <div className="flex gap-2 items-center">
-                    <button className="text-2xl xl:text-4xl">
-                        <ion-icon name="volume-off-outline"></ion-icon>
-                    </button>
-                    <input type="range" min="0" max="100" step="10" defaultValue="50" className="appearance-none cursor-pointer bg-gray-200 rounded-lg h-2" />
-                </div>
-                <button className="text-2xl xl:text-4xl">
-                    <ion-icon name="play-outline"></ion-icon>
-                </button>
-                <button className="text-2xl xl:text-4xl">
-                    <ion-icon name="pause-outline"></ion-icon>
-                </button>
-                <button className="text-2xl xl:text-4xl">
-                    <ion-icon name="scan-outline"></ion-icon>
-                </button>
-                <button className="text-2xl xl:text-4xl">
-                    <ion-icon name="share-social-outline"></ion-icon>
-                </button>
-            </div>
+            <Controls names={ ["volume-off-outline", "play-outline", "pause-outline", "scan-outline", "share-social-outline"] } />
         </div>
     );
 };
