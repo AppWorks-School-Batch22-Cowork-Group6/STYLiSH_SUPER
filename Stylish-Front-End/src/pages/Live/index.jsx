7f14b47d446ab872f190b4e9176a5393a50f2103
Sailwayfun <liaoleon000513@gmail.com>
2023-11-05 11:51:15 +0800

    Style the icons and input range

--- Stylish-Front-End/src/pages/Live/index.jsx
+++ Stylish-Front-End/src/pages/Live/index.jsx
@@ -5,18 +5,22 @@ const Live = () => {
         <Wrapper>
             <Video />
             <div className="flex gap-4 mt-4">
-                <button className="text-2xl">
-                    <ion-icon name="volume-off-outline"></ion-icon>
-                </button>
-                <input type="range" min="0" max="100" step="10" defaultValue="50" />
-                <button>
+                <div className="flex gap-2 ml-4 items-center">
+                    <button className="text-2xl xl:text-4xl">
+                        <ion-icon name="volume-off-outline"></ion-icon>
+                    </button>
+                    <input type="range" min="0" max="100" step="10" defaultValue="50" className="appearance-none cursor-pointer bg-gray-200 rounded-lg h-2" />
+                </div>
+                <button className="text-2xl xl:text-4xl">
                     <ion-icon name="play-outline"></ion-icon>
+                </button>
+                <button className="text-2xl xl:text-4xl">
                     <ion-icon name="pause-outline"></ion-icon>
                 </button>
-                <button>
+                <button className="text-2xl xl:text-4xl">
                     <ion-icon name="scan-outline"></ion-icon>
                 </button>
-                <button>
+                <button className="text-2xl xl:text-4xl">
                     <ion-icon name="share-social-outline"></ion-icon>
                 </button>
             </div>
