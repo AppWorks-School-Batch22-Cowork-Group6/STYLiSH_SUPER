559ea2cd1925bc85422f923ee7ff1c44ac4d7a6a
Sailwayfun <liaoleon000513@gmail.com>
2023-11-05 11:32:24 +0800

    Add IonIcons web components

--- Stylish-Front-End/src/pages/Live/index.jsx
+++ Stylish-Front-End/src/pages/Live/index.jsx
@@ -4,14 +4,21 @@ const Live = () => {
     return (
         <Wrapper>
             <Video />
-            <div className="flex gap-4">
-                <button>
-                    volume
-                    <input type="range" min="0" max="100" step="10" defaultValue="50" />
+            <div className="flex gap-4 mt-4">
+                <button className="text-2xl">
+                    <ion-icon name="volume-off-outline"></ion-icon>
                 </button>
-                <button>play</button>
-                <button>full screen</button>
-                <button>share</button>
+                <input type="range" min="0" max="100" step="10" defaultValue="50" />
+                <button>
+                    <ion-icon name="play-outline"></ion-icon>
+                    <ion-icon name="pause-outline"></ion-icon>
+                </button>
+                <button>
+                    <ion-icon name="scan-outline"></ion-icon>
+                </button>
+                <button>
+                    <ion-icon name="share-social-outline"></ion-icon>
+                </button>
             </div>
         </Wrapper>
     );
