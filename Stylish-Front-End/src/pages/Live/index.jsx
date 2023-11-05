075552ff26e67fb86ca42c420825973fb0a30492
Sailwayfun <liaoleon000513@gmail.com>
2023-11-05 11:04:22 +0800

    Create icon markups

--- Stylish-Front-End/src/pages/Live/index.jsx
+++ Stylish-Front-End/src/pages/Live/index.jsx
@@ -4,6 +4,15 @@ const Live = () => {
     return (
         <Wrapper>
             <Video />
+            <div className="flex gap-4">
+                <button>
+                    volume
+                    <input type="range" min="0" max="100" step="10" defaultValue="50" />
+                </button>
+                <button>play</button>
+                <button>full screen</button>
+                <button>share</button>
+            </div>
         </Wrapper>
     );
 };
